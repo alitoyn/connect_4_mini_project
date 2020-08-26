3f739cdc84e0eec3398e1698d7de182e8660883a
Ali Toyn <ali.toyn@infinityworks.com>
2020-08-26 15:01:05 +0100

    commiting work completed by John H and Ali on Aug 26th

---
+++ README.md
@@ -0,0 +1,7 @@
+# Code Structure
+
+- Create html, css and js files
+- setup blank grid (put it in a table?)
+- grid lines 
+- place item in clicked space
+
---
+++ connect4.js
@@ -0,0 +1,28 @@
+for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+        const gridPosition = document.getElementById("row-" + i + "-column-" + j);
+        //gridPosition.addEventListener("click", positionClick.bind(null, i, j));
+
+
+    }
+}
+
+function resetClick(event) {
+    console.log('clicked')
+    document.getElementById("row-0-column-0").innerText = "❌"
+}
+
+// Bind the click event for the reset button.
+const button0 = document.getElementById("button0");
+const button1 = document.getElementById("button1");
+const button2 = document.getElementById("button2");
+const button3 = document.getElementById("button3");
+const button4 = document.getElementById("button4");
+const button5 = document.getElementById("button5");
+const button6 = document.getElementById("button6");
+
+resetButton.addEventListener("click", resetClick);
+resetButton.addEventListener("click", resetClick);
+resetButton.addEventListener("click", resetClick);
+resetButton.addEventListener("click", resetClick);
+resetButton.addEventListener("click", resetClick);
