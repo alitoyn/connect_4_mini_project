495ab8023bf0afcf255c09c1218edf6f1e9ca37b
John Holden <john.holden@infinityworks.com>
2020-08-27 10:56:00 +0100

    basic outline

--- connect4.js
+++ connect4.js
@@ -1,14 +1,31 @@
-for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < 3; j++) {
-        const gridPosition = document.getElementById("row-" + i + "-column-" + j);
-        //gridPosition.addEventListener("click", positionClick.bind(null, i, j));
+for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 7; j++) {
+        document.getElementById("row-" + i + "-column-" + j).innerText = '🔵';
     }
 }
 
-function resetClick(event) {
-    console.log('clicked')
-    document.getElementById("row-0-column-0").innerText = "❌"
+function resetBoard() {
+    for (let i = 0; i < 5; i++) {
+        for (let j = 0; j < 7; j++) {
+            document.getElementById("row-" + i + "-column-" + j).innerText = '🔵';
+        }
+    }
 }
+
+function buttonClick(event) {
+    console.log(event.target.id)
+    let button = event.target.id
+    if (button === 'reset') {
+        console.log('i have been clicked')
+        resetBoard()
+    } else {
+        button = button[button.length - 1]
+        let i = 0
+        document.getElementById("row-" + i + "-column-" + button).innerText = '🟡'
+    }
+}
+
+
 
 // Bind the click event for the reset button.
 const button0 = document.getElementById("button0");
@@ -18,8 +35,13 @@ const button3 = document.getElementById("button3");
 const button4 = document.getElementById("button4");
 const button5 = document.getElementById("button5");
 const button6 = document.getElementById("button6");
-button0.addEventListener("click", resetClick);
-resetButton.addEventListener("click", resetClick);
-resetButton.addEventListener("click", resetClick);
-resetButton.addEventListener("click", resetClick);
-resetButton.addEventListener("click", resetClick);
+const reset = document.getElementById("reset");
+
+button0.addEventListener("click", buttonClick);
+button1.addEventListener("click", buttonClick);
+button2.addEventListener("click", buttonClick);
+button3.addEventListener("click", buttonClick);
+button4.addEventListener("click", buttonClick);
+button5.addEventListener("click", buttonClick);
+button6.addEventListener("click", buttonClick);
+reset.addEventListener("click", buttonClick);
