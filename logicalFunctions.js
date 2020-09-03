2e71c554b5f2dc5509471142f73de4307a2f1539
Ali Toyn <ali.toyn@infinityworks.com>
2020-09-03 13:41:14 +0100

    finished changing file structure

--- logicalFunctions.js
+++ logicalFunctions.js
@@ -98,32 +98,6 @@ function checkDiagsNegative(row, column) {
   return false;
 }
 
-function updateHTML(board) {
-  // update buttons with current player token
-  for (let i = 0; i < cols; i++) {
-    const buttonInnerText = playerCount % 2 === 0 ? '🟡' : '🔴';
-    $(`#button${i}`).text(buttonInnerText);
-  }
-
-  // match the html board to the array
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-      if (board[i][j] === null) {
-        $(`#row-${i}-column-${j}`).text('🟣');
-      } else if (board[i][j] === 'y') {
-        $(`#row-${i}-column-${j}`).text('🟡');
-      } else {
-        $(`#row-${i}-column-${j}`).text('🔴');
-      }
-    }
-  }
-}
-
-function winnerNotification(winner) {
-  const player = winner === 'y' ? 'yellow' : 'red';
-  $('#grid').css('background-color', player);
-}
-
 function returnLastChar(string) {
   return string[string.length - 1];
 }
