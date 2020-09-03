05a8780201a96559ee111331612aa7fa89c0e8d0
Ali Toyn <ali.toyn@infinityworks.com>
2020-09-03 12:02:53 +0100

    fixed linter problems in test file

--- connect4.test.js
+++ connect4.test.js
@@ -1,88 +1,94 @@
-// check cols
+const each = require('jest-each').default;
 const { checkCols, getBoard, checkRows } = require('./functions.js');
-const each = require("jest-each").default;
 
-describe("token in empty board", () => {
+describe('token in empty board', () => {
+  const t1Counter = 'y';
+  const t2Counter = 'r';
+  const expected = false;
 
-    let t1_counter = 'y'
-    let t2_counter = 'r'
-    let expected = false;
+  each([
+    ['Place yellow in empty board', t1Counter, expected],
+    ['Place red in empty board', t2Counter, expected],
 
-    each([
-        ["Place yellow in empty board", t1_counter, expected],
-        ["Place red in empty board", t2_counter, expected],
+  ]).it("'%s'", (text, counter, expectedOutput) => {
+    const rows = 6;
+    const cols = 7;
+    const connectN = 4;
+    const board = getBoard(rows, cols);
+    const placedCounter = [0, 0];
+    board[0][0] = counter;
 
-
-    ]).it("'%s'", (text, counter, expected_output) => {
-        let rows = 6;
-        let cols = 7;
-        let connectN = 4;
-        let board = getBoard(rows, cols);
-        let placedCounter = [0, 0]
-        board[0][0] = counter;
-
-        expect(checkCols(placedCounter[0], placedCounter[1], board, connectN)).toStrictEqual(expected_output);
-
-
-    });
+    expect(
+      checkCols(placedCounter[0],
+        placedCounter[1],
+        board,
+        connectN),
+    )
+      .toStrictEqual(expectedOutput);
+  });
 });
 
-describe("column in empty board", () => {
+describe('column in empty board', () => {
+  const expected = true;
+  const counter1 = 'y';
+  const counter2 = 'r';
 
-    let expected = true;
-    counter1 = 'y'
-    counter2 = 'r'
+  each([
+    ['yellow column in empty board', counter1, expected],
+    ['red column in empty board', counter2, expected],
 
-    each([
-        ["yellow column in empty board", counter1, expected],
-        ["red column in empty board", counter2, expected],
+  ]).it("'%s'", (text, counter, expectedOutput) => {
+    const rows = 6;
+    const cols = 7;
+    const connectN = 4;
+    const board = getBoard(rows, cols);
+    board[0][0] = counter;
+    board[1][0] = counter;
+    board[2][0] = counter;
+    board[3][0] = counter;
 
-    ]).it("'%s'", (text, counter, expected_output) => {
-        let rows = 6;
-        let cols = 7;
-        let connectN = 4;
-        let board = getBoard(rows, cols);
-        board[0][0] = counter
-        board[1][0] = counter
-        board[2][0] = counter
-        board[3][0] = counter
+    const placedCounter = [3, 0];
 
-        let placedCounter = [3, 0]
-
-        expect(checkCols(placedCounter[0], placedCounter[1], board, connectN)).toStrictEqual(expected_output);
-
-    });
+    expect(
+      checkCols(placedCounter[0],
+        placedCounter[1],
+        board,
+        connectN),
+    )
+      .toStrictEqual(expectedOutput);
+  });
 });
 
 // check rows
 
-describe("row in empty board", () => {
+describe('row in empty board', () => {
+  const expected = true;
+  counter1 = 'y';
+  counter2 = 'r';
 
-    let expected = true;
-    counter1 = 'y'
-    counter2 = 'r'
+  each([
+    ['yellow row in empty board', counter1, expected],
+    ['red row in empty board', counter2, expected],
 
-    each([
-        ["yellow row in empty board", counter1, expected],
-        ["red row in empty board", counter2, expected],
+  ]).it("'%s'", (text, counter, expectedOutput) => {
+    const rows = 6;
+    const cols = 7;
+    const connectN = 4;
+    const board = getBoard(rows, cols);
 
-    ]).it("'%s'", (text, counter, expected_output) => {
-        let rows = 6;
-        let cols = 7;
-        let connectN = 4;
-        let board = getBoard(rows, cols);
+    board[0][0] = counter;
+    board[0][1] = counter;
+    board[0][2] = counter;
+    board[0][3] = counter;
 
-        board[0][0] = counter
-        board[0][1] = counter
-        board[0][2] = counter
-        board[0][3] = counter
+    const placedCounter = [0, 3];
 
-        let placedCounter = [0, 3]
-
-        expect(checkRows(placedCounter[0], placedCounter[1], board, connectN)).toStrictEqual(expected_output);
-
-    });
+    expect(
+      checkRows(placedCounter[0],
+        placedCounter[1],
+        board,
+        connectN),
+    )
+      .toStrictEqual(expectedOutput);
+  });
 });
-
-
-// check diagonals
