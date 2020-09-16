eb5d5c690ccec88180611a71164f042e923a48a9
Ali Toyn <ali.toyn@infinityworks.com>
2020-09-16 11:54:36 +0100

    guest user button creates a random user in the backend

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -31,6 +31,37 @@ $(() => {
       });
     });
   $('#exampleModal').modal({ backdrop: 'static', keyboard: false });
+
+  $('#guest-button')
+    .click(() => {
+      $.ajax({
+        url: 'https://randomuser.me/api/?inc=name',
+        dataType: 'json',
+        success: (data) => {
+          const user = data.results[0].name.first + ' ' + data.results[0].name.last;
+          const pass = '123';
+          const body = {
+            username: user,
+            password: pass,
+          };
+          $.ajax({
+            method: 'POST',
+            url: api + '/login',
+            dataType: 'json',
+            data: JSON.stringify(body),
+            contentType: 'application/json',
+            success: async (res) => {
+              loadHTML(res);
+              $('#exampleModal').modal('hide');
+            },
+            error: (res) => {
+              $('#error-message').css('display', 'inline');
+            },
+          });
+        },
+      });
+    });
+  $('#exampleModal').modal({ backdrop: 'static', keyboard: false });
 });
 
 // event loop
