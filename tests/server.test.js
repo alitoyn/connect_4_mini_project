952320dc9c13fbe7c0ff34c78ab688defab477fe
Ali Toyn <ali.toyn@infinityworks.com>
2020-09-18 20:45:10 +0100

    chnaged test formatting

--- tests/server.test.js
+++ tests/server.test.js
@@ -75,8 +75,12 @@ afterEach(() => {
 describe('/', () => {
   it('returns a html file', async (done) => {
     const response = await request.get('/')
-      .expect(200)
       .expect('Content-Type', 'text/html; charset=UTF-8');
+    done();
+  });
+  it('returns a 200 success code', async (done) => {
+    const response = await request.get('/')
+      .expect(200);
     done();
   });
 });
@@ -103,11 +107,18 @@ describe('/reset', () => {
   it('returns 200', async (done) => {
     const response = await request.get('/reset')
       .set('Cookie', ['token=RandomToken'])
-      .expect(200)
+      .expect(200);
+    done();
+  });
+
+  it('returns json', async (done) => {
+    const response = await request.get('/reset')
+      .set('Cookie', ['token=RandomToken'])
       .expect('Content-Type', 'application/json; charset=utf-8');
     done();
   });
 
+  test.todo('check the body content returned is correct');
   test.todo('refactor reset for no matching token and test');
 });
 
@@ -115,74 +126,112 @@ describe('/reset-scores', () => {
   it('returns 200', async (done) => {
     const response = await request.get('/reset-scores')
       .set('Cookie', ['token=RandomToken'])
-      .expect(200)
+      .expect(200);
+    done();
+  });
+
+  it('returns json', async (done) => {
+    const response = await request.get('/reset-scores')
+      .set('Cookie', ['token=RandomToken'])
       .expect('Content-Type', 'application/json; charset=utf-8');
     done();
   });
 });
 
 describe('/move', () => {
+  const body = {
+    button: '0',
+  };
+
   it('returns 200', async (done) => {
-    const body = {
-      button: '0',
-    };
     const response = await request
       .post('/move')
       .send(body)
       .set('Cookie', ['token=RandomToken'])
-      .expect(200)
+      .expect(200);
+    done();
+  });
+
+  it('returns json', async (done) => {
+    const response = await request
+      .post('/move')
+      .send(body)
+      .set('Cookie', ['token=RandomToken'])
       .expect('Content-Type', 'application/json; charset=utf-8');
     done();
   });
 
-  it('returns error if board is full', async (done) => {
-    const body = {
-      button: '0',
-    };
+  it('returns error 400 if board is full', async (done) => {
     const response = await request
       .post('/move')
       .send(body)
       .set('Cookie', ['token=lastTurnTest_token'])
-      .expect(406)
+      .expect(400)
       .expect('Content-Type', 'application/json; charset=utf-8')
       .expect('"The selected column is full"');
     done();
   });
-  test.todo('move errors');
+
+  it('returns the correct error message', async (done) => {
+    const response = await request
+      .post('/move')
+      .send(body)
+      .set('Cookie', ['token=lastTurnTest_token'])
+      .expect('"The selected column is full"');
+    done();
+  });
+
+  test.todo('different move errors');
 });
 
 describe('/login', () => {
-  it('returns 200', async (done) => {
-    const body = {
-      username: 'otherCoolUsername',
-      password: 'password123',
-    };
+  const body = {
+    username: 'otherCoolUsername',
+    password: 'password123',
+  };
 
-    const data = {
-      board: [[null, null, null, null, null, null, null],
-        [null, null, null, null, null, null, null],
-        [null, null, null, null, null, null, null],
-        [null, null, null, null, null, null, null],
-        [null, null, null, null, null, null, null],
-        [null, null, null, null, null, null, null]],
-      name: 'otherCoolUsername',
-      rows: 6,
-      cols: 7,
-      turnCount: 0,
-      winner: false,
-      draw: false,
-      winCondition: 4,
-      player1Score: 0,
-      player2Score: 0,
-    };
+  const data = {
+    board: [[null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null]],
+    name: 'otherCoolUsername',
+    rows: 6,
+    cols: 7,
+    turnCount: 0,
+    winner: false,
+    draw: false,
+    winCondition: 4,
+    player1Score: 0,
+    player2Score: 0,
+  };
+
+  it('returns 200', async (done) => {
     const response = await request
       .post('/login')
       .send(body)
-      .expect(200)
-      .expect('Content-Type', 'application/json; charset=utf-8')
+      .expect(200);
+    done();
+  });
+
+  it('returns a json', async (done) => {
+    const response = await request
+      .post('/login')
+      .send(body)
+      .expect('Content-Type', 'application/json; charset=utf-8');
+    done();
+  });
+
+  it('returns the correct body data', async (done) => {
+    const response = await request
+      .post('/login')
+      .send(body)
       .expect(data);
     done();
   });
+
   test.todo('login errors');
 });
 
